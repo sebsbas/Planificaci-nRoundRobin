4befcecf1d063d3390f511791eedb27813a842c5
Daniel <dafero26>
2022-10-18 17:20:32 -0500

    Creditos

---
+++ app/src/main/java/com/example/planificacinroundrobin/Creditos.java
@@ -0,0 +1,76 @@
+package com.example.planificacinroundrobin;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class Creditos extends AppCompatActivity {
+
+    private ImageButton btnNext;
+    private ImageButton btnRestore;
+    private ImageButton btnHome;
+    private EditText text;
+
+    ActivityResultLauncher<Intent> activityLauncher=registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                }
+            }
+    );
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creditos);
+
+
+        btnNext = (ImageButton) findViewById(R.id.btnBack6);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Entró");
+                try {
+                    Intent intent = new Intent(Creditos.this, Tehory5.class);
+
+                    activityLauncher.launch(intent);
+                }catch (Exception e){
+                    e.toString();
+                }
+
+
+            }
+        });
+
+        btnRestore = (ImageButton) findViewById(R.id.btnBack6);
+        btnRestore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Creditos.this, Tehory3.class);
+
+                activityLauncher.launch(intent);
+
+            }
+        });
+
+        btnHome = (ImageButton) findViewById(R.id.btnHome6);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Creditos.this, MainActivity.class);
+
+                activityLauncher.launch(intent);
+
+            }
+        });
+    }
+}
