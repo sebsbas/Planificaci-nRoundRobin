5b326d0dc8efed4b99e8068cb91d8b82578eb7f5
DanisLozano01 <davalode@gmail.com>
2022-10-18 21:56:24 -0500

    Cambios de color y referencias

--- app/src/main/java/com/example/planificacinroundrobin/Referencias.java
+++ app/src/main/java/com/example/planificacinroundrobin/Referencias.java
@@ -1,23 +1,18 @@
 package com.example.planificacinroundrobin;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-public class Creditos extends AppCompatActivity {
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
-    private ImageButton btnNext;
-    private ImageButton btnRestore;
-    private ImageButton btnHome;
-    private EditText text;
+public class Referencias extends AppCompatActivity {
 
     ActivityResultLauncher<Intent> activityLauncher=registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -31,41 +26,38 @@ public class Creditos extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_creditos);
+        setContentView(R.layout.activity_referencias);
 
+        EditText text = (EditText) findViewById(R.id.txtReferencias);
+        text.setKeyListener(null);
 
-        btnNext = (ImageButton) findViewById(R.id.btnBack6);
+        ImageButton btnNext = (ImageButton) findViewById(R.id.btnNext7);
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    Intent intent = new Intent(Creditos.this, Tehory5.class);
-
-                    activityLauncher.launch(intent);
-                }catch (Exception e){
-                    e.toString();
-                }
-
-
-            }
-        });
-
-        btnRestore = (ImageButton) findViewById(R.id.btnBack6);
-        btnRestore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Creditos.this, Example2.class);
+                Intent intent = new Intent(Referencias.this, Example.class);
 
                 activityLauncher.launch(intent);
 
             }
         });
 
-        btnHome = (ImageButton) findViewById(R.id.btnHome7);
+        ImageButton btnRestore = (ImageButton) findViewById(R.id.btnBack7);
+        btnRestore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Referencias.this, Referencias.class);
+
+                activityLauncher.launch(intent);
+
+            }
+        });
+
+        ImageButton btnHome = (ImageButton) findViewById(R.id.btnHome7);
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Creditos.this, MainActivity.class);
+                Intent intent = new Intent(Referencias.this, MainActivity.class);
 
                 activityLauncher.launch(intent);
 
